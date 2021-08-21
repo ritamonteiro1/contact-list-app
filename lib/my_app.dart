19db94bda180503c0e8dd59919f773b809562ee3
ritamonteiro1 <ritamonteiro@tokenlab.com.br>
2021-08-21 11:48:07 -0300

    create class HomeView

--- lib/my_app.dart
+++ lib/my_app.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:list_dio_app/home_view.dart';
 
 class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      home: HomeView()
+    );
   }
 }
